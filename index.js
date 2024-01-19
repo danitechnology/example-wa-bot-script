6adc2c9a0f8db50a59b9bd21f3ef8343f913fca6
Dani Technology <153726864+danitechnology@users.noreply.github.com>
2024-01-19 09:44:14 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 const wwa = require('./whatsapp-web-api');
-const fs = require('fs');
-const chalk = require('chalk');
 const config = require('./config/settings.js');
 const client = require('./includes/client.js');
 
